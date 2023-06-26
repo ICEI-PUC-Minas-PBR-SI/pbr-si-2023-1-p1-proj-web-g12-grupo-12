ffcc8a18521a90f8d13dd08c1f00d042ed8d6e27
thivizinx <75588336+thivizinx@users.noreply.github.com>
2023-06-25 21:35:33 -0300

    registrar noticia

--- principal.js
+++ principal.js
@@ -18,3 +18,30 @@ function initMap() {
 window.onload = function () {
     initMap();
 };
+document.getElementById('newsForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+    
+    // Obter os valores dos campos do formulário
+    var newsId = document.getElementById('newsId').value;
+    var newsTitle = document.getElementById('newsTitle').value;
+    var newsContent = document.getElementById('newsContent').value;
+    var authorName = document.getElementById('authorName').value;
+    
+    // Criar um objeto de notícia com os valores obtidos
+    var news = {
+      id: newsId,
+      title: newsTitle,
+      content: newsContent,
+      author: authorName
+    };
+    
+    // Exibir a notícia registrada no console (você pode substituir esta linha com o código para enviar a notícia para um servidor, por exemplo)
+    console.log('Notícia registrada:', news);
+    
+    // Limpar os campos do formulário
+    document.getElementById('newsId').value = '';
+    document.getElementById('newsTitle').value = '';
+    document.getElementById('newsContent').value = '';
+    document.getElementById('authorName').value = '';
+  });
+  
